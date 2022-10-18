22a08e85c1e3606b1be4919dc0aaa76940aa572b
James Mburu Githire <jamesmburug24@gmail.com>
2022-10-18 08:09:39 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+    if(array.length == 1)
+        array.join()
+    elsif (array.length == 2)
+        array.join(" and ")
+    elsif (array.length > 2)
+        new_arr = array.slice(0, array.length-1).join(", ")
+        [new_arr, array[array.length-1]].join(", and ")
+    end
